56f511bcf0910b2944214aa87a4ae2188102feee
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-16 15:07:27 +0600

    app structred

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('hello from nodejs');
+// import routes
+const userRoutes = require('./routes/user');
+
+// db connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected'));
+
+mongoose.connection.on('error', (err) => {
+  console.log(`DB connection error: ${err.message}`);
 });
+
+// routes middleware
+app.use('/api', userRoutes);
 
 const port = process.env.PORT || 5000;
 
---
+++ routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { sayHi } = require('../controllers/user');
+
+router.get('/', sayHi);
+
+module.exports = router;
