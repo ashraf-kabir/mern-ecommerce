b84c289f4d7e6d0c730860049142f6fe3b7fff90
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-16 19:00:26 +0600

    user signput implemented

--- controllers/user.js
+++ controllers/user.js
@@ -45,3 +45,8 @@ exports.signin = (req, res) => {
     return res.json({ token, user: { _id, email, name, role } });
   });
 };
+
+exports.signout = (req, res) => {
+  res.clearCookie('t');
+  res.json({ message: 'Signout success' });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, signout } = require('../controllers/user');
 const { userSignupValidator } = require('../validator');
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get('/signout', signout);
 
 module.exports = router;
