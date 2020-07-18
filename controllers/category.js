b0fbf83788eaacb03887053f4f77c339a2b39816
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-18 14:01:39 +0600

    product and category added

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,18 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "Category doesn't exist",
+      });
+    }
+    req.category = category;
+    next();
+  });
+};
+
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
@@ -10,5 +22,49 @@ exports.create = (req, res) => {
       });
     }
     res.json({ data });
+  });
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.update = (req, res) => {
+  // console.log('req.body', req.body);
+  // console.log('category update param', req.params.categoryId);
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+exports.remove = (req, res) => {
+  const category = req.category;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: 'Category deleted',
+    });
+  });
+};
+
+exports.list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
   });
 };
