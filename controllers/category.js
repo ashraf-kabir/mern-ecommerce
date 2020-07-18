f219f0022755f68f6b3199e221bdd06cc0e3a031
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-18 11:39:23 +0600

    category added

---
+++ controllers/category.js
@@ -0,0 +1,14 @@
+const Category = require('../models/category');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+
+exports.create = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({ data });
+  });
+};
