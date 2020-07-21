e9bd7bf6ed8a5a1a05eaa697192706ad2dc21f3f
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-21 18:18:35 +0600

    user signin and signup frontend done

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -1,8 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
+import { signin, authenticate } from '../auth';
 
-const Signin = () => (
-  <Layout title='Signin page' description='Signin to MERN E-commerce App'></Layout>
-);
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: 'kroos@gmail.com',
+    password: '12345678',
+    error: '',
+    loading: false,
+    redirectToReferrer: false,
+  });
+
+  const { email, password, loading, error, redirectToReferrer } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault(); // so that browser does not reload
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, loading: false });
+      } else {
+        authenticate(data, () => {
+          setValues({
+            ...values,
+            redirectToReferrer: true,
+          });
+        });
+      }
+    });
+  };
+
+  const signInForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={handleChange('email')}
+          type='email'
+          className='form-control'
+          value={email}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={handleChange('password')}
+          type='password'
+          className='form-control'
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className='alert alert-danger'
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
+
+  const showLoading = () =>
+    loading && (
+      <div className='alert alert-info'>
+        <h2>Loading...</h2>
+      </div>
+    );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to='/' />;
+    }
+  };
+
+  return (
+    <Layout
+      title='Signup page'
+      description='Signup to MERN E-commerce App'
+      className='container col-md-8 offset-md-2'
+    >
+      {showLoading()}
+      {showError()}
+      {signInForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 
 export default Signin;
