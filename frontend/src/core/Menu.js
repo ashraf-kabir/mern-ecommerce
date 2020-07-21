6114b136e69a538077ef3475453f5484d74dce63
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-21 23:09:54 +0600

    private and admin route with user

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -18,15 +18,30 @@ const Menu = ({ history }) => (
           Home
         </Link>
       </li>
-      <li className='nav-item'>
-        <Link
-          className='nav-link'
-          style={isActive(history, '/user/dashboard')}
-          to='/user/dashboard'
-        >
-          Dashboard
-        </Link>
-      </li>
+      {isAuthenticated() && isAuthenticated().user.role === 0 && (
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, '/user/dashboard')}
+            to='/user/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+
+      {isAuthenticated() && isAuthenticated().user.role === 1 && (
+        <li className='nav-item'>
+          <Link
+            className='nav-link'
+            style={isActive(history, '/admin/dashboard')}
+            to='/admin/dashboard'
+          >
+            Dashboard
+          </Link>
+        </li>
+      )}
+
       {!isAuthenticated() && (
         <Fragment>
           <li className='nav-item'>
