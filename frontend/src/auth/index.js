c8e672e6ce03ca7d12fc5bf91dad1eeed2a21648
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-21 17:42:16 +0600

    signup frontend implemented

---
+++ frontend/src/auth/index.js
@@ -0,0 +1,19 @@
+import { API } from '../config';
+
+export const signup = (user) => {
+  // console.log(name, email, password);
+  return fetch(`${API}/signup`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
