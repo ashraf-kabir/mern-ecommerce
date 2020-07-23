a7e208d1dd7ad760664190a389d89b1e4ecaa0ee
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-23 17:41:49 +0600

    displaing categories and products done

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
+import { getProducts } from './apiCore';
+import Card from './Card';
 
-const Home = () => (
-  <Layout title='Home page' description='MERN E-commerce App'></Layout>
-);
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState([]);
+
+  const loadProductsBySell = () => {
+    getProducts('sold').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts('createdAt').then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsByArrival();
+    loadProductsBySell();
+  }, []);
+
+  return (
+    <Layout
+      title='Home page'
+      description='MERN E-commerce App'
+      className='container-fluid'
+    >
+      <h2 className='mb-4'>New Arrivals</h2>
+      <div className='row'>
+        {productsByArrival.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+
+      <h2 className='mb-4'>Best Sellers</h2>
+      <div className='row'>
+        {productsBySell.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Home;
---
+++ frontend/src/core/apiCore.js
@@ -0,0 +1,11 @@
+import { API } from '../config';
+
+export const getProducts = (sortBy) => {
+  return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
+    method: 'GET',
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
