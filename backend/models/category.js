d50e10a26dc9d49e5a58522ad3833934e05e8970
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-23 11:23:23 +0600

    add category done

--- backend/models/category.js
+++ backend/models/category.js
@@ -7,6 +7,7 @@ const categorySchema = new mongoose.Schema(
       trim: true,
       required: true,
       maxlength: 32,
+      unique: true,
     },
   },
   { timestamps: true }
---
+++ frontend/src/admin/apiAdmin.js
@@ -0,0 +1,19 @@
+import { API } from '../config';
+
+export const createCategory = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
