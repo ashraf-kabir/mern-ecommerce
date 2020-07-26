a5f2691bf101e2505216ecb8f9cb88a186798a98
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-26 22:27:38 +0600

    single product details implemented

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -6,15 +6,17 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 const product = require('../models/product');
 
 exports.productById = (req, res, next, id) => {
-  Product.findById(id).exec((err, product) => {
-    if (err || !product) {
-      return res.status(400).json({
-        error: 'Product not found',
-      });
-    }
-    req.product = product;
-    next();
-  });
+  Product.findById(id)
+    .populate('category')
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: 'Product not found',
+        });
+      }
+      req.product = product;
+      next();
+    });
 };
 
 exports.read = (req, res) => {
