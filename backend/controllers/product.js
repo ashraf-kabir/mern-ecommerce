86ed77306f196300dcfa6d92e691bc987f92bc15
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-26 13:21:59 +0600

    search product implemented

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -255,3 +255,26 @@ exports.photo = (req, res, next) => {
   }
   next();
 };
+
+exports.listSearch = (req, res) => {
+  // create query object to hold search value
+  const query = {};
+  // assign search value to query.name
+  if (req.query.search) {
+    query.name = { $regex: req.query.search, $options: 'i' };
+    // assign category value to query.category
+    if (req.query.category && req.query.category != 'All') {
+      query.category = req.query.category;
+    }
+    // find the product based on query object with 2 properties
+    // search and category
+    Product.find(query, (err, products) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(products);
+    }).select('-photo');
+  }
+};
--- frontend/src/core/apiCore.js
+++ frontend/src/core/apiCore.js
@@ -1,4 +1,5 @@
 import { API } from '../config';
+import queryString from 'query-string';
 
 export const getProducts = (sortBy) => {
   return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -40,4 +41,16 @@ export const getFilteredProducts = (skip, limit, filters = {}) => {
     .catch((err) => {
       console.log(err);
     });
+};
+
+export const list = (params) => {
+  const query = queryString.stringify(params);
+  console.log('query', query);
+  return fetch(`${API}/products/search?${query}`, {
+    method: 'GET',
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
 };
