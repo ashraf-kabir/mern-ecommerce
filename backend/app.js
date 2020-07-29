6850d7dc1b720634460cb687d925480305425db7
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-29 17:03:27 +0600

    payment gateway completed

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
 
 // app
 const app = express();
@@ -39,6 +40,7 @@ app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
 
 const port = process.env.PORT || 5000;
 
--- frontend/src/core/cartHelpers.js
+++ frontend/src/core/cartHelpers.js
@@ -79,3 +79,10 @@ export const removeItem = (productId) => {
   }
   return cart;
 };
+
+export const emptyCart = (next) => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('cart');
+    next();
+  }
+};
