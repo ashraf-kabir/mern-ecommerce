b9a2e3639460b01bb326898d11e9d9b91bffca85
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-31 16:29:39 +0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 const expressValidator = require('express-validator');
 require('dotenv').config();
 // import routes
@@ -13,8 +14,6 @@ const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const braintreeRoutes = require('./routes/braintree');
 const orderRoutes = require('./routes/order');
-
-const path = require('path');
 
 // app
 const app = express();
