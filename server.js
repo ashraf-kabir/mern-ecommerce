07f06400d700af04f93f892e01306eaf7908312d
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-07-31 22:56:16 +0600

    updated

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const expressValidator = require('express-validator');
+const connectDB = require('./config/db');
 require('dotenv').config();
 // import routes
 const authRoutes = require('./routes/auth');
@@ -23,16 +24,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // db connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB Connected'));
+// mongoose
+//   .connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('MongoDB Connected'));
 
-mongoose.connection.on('error', (err) => {
-  console.log(`DB connection error: ${err.message}`);
-});
+// mongoose.connection.on('error', (err) => {
+//   console.log(`DB connection error: ${err.message}`);
+// });
+
+connectDB();
 
 // middlewares
 app.use(morgan('dev'));
