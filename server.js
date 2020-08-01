535c0190d854e5de4712e27a208bfbe9774d4415
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-01 13:16:55 +0600

    updated

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
