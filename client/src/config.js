36585c62f3ff7dc94ff2e9c68ebbf4b5f30cbbc4
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-01 16:18:15 +0600

    updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,1 @@
-export const API = process.env.NODE_ENV === 'production' ? 'https://ecommerce-ak.herokuapp.com/' : process.env.REACT_APP_API_URL;
-// https://ecommerce-ak.herokuapp.com/
+export const API = process.env.REACT_APP_API_URL;
