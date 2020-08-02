520d4fa2277919df9cb575847b98b2b71e028b84
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-02 13:56:13 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const expressValidator = require('express-validator');
-// require('dotenv').config();
+require('dotenv').config();
 // import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
