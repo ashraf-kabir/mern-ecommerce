f32e3721786f7d3aa101485d54f57e853b0d82d2
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-04 20:36:17 +0600

    link updated

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -7,7 +7,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -165,7 +164,7 @@ export default function Signin() {
               </Link>
             </Grid>
             <Grid item>
-              <Link href='/signup' variant='body2'>
+              <Link to='/signup' variant='body2'>
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-
+import { Link } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+// import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -164,7 +164,7 @@ export default function Signup() {
           </Button>
           <Grid container justify='flex-end'>
             <Grid item>
-              <Link href='/signin' variant='body2'>
+              <Link to='/signin' variant='body2'>
                 Already have an account? Sign in
               </Link>
             </Grid>
