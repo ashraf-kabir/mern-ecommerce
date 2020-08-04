267405e5020a03a922edf7c2c8a65e195de885d9
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-04 16:58:06 +0600

    Update Signup.js

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,9 +1,57 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+// import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { signup } from '../auth';
 
-const Signup = () => {
+function Copyright() {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='#'>
+        MERN E-commerce
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Signup() {
   const [values, setValues] = useState({
     name: '',
     email: '',
@@ -37,41 +85,124 @@ const Signup = () => {
     }); // sending js object
   };
 
+  const classes = useStyles();
+
   const signUpForm = () => (
-    <form>
-      <div className='form-group'>
-        <label className='text-muted'>Name</label>
-        <input
-          onChange={handleChange('name')}
-          type='text'
-          className='form-control'
-          value={name}
-        />
-      </div>
+    // <form>
+    //   <div className='form-group'>
+    //     <label className='text-muted'>Name</label>
+    //     <input
+    //       onChange={handleChange('name')}
+    //       type='text'
+    //       className='form-control'
+    //       value={name}
+    //     />
+    //   </div>
 
-      <div className='form-group'>
-        <label className='text-muted'>Email</label>
-        <input
-          onChange={handleChange('email')}
-          type='email'
-          className='form-control'
-          value={email}
-        />
-      </div>
+    //   <div className='form-group'>
+    //     <label className='text-muted'>Email</label>
+    //     <input
+    //       onChange={handleChange('email')}
+    //       type='email'
+    //       className='form-control'
+    //       value={email}
+    //     />
+    //   </div>
 
-      <div className='form-group'>
-        <label className='text-muted'>Password</label>
-        <input
-          onChange={handleChange('password')}
-          type='password'
-          className='form-control'
-          value={password}
-        />
+    //   <div className='form-group'>
+    //     <label className='text-muted'>Password</label>
+    //     <input
+    //       onChange={handleChange('password')}
+    //       type='password'
+    //       className='form-control'
+    //       value={password}
+    //     />
+    //   </div>
+    //   <button onClick={clickSubmit} className='btn btn-primary'>
+    //     Submit
+    //   </button>
+    // </form>
+    <Container component='main' maxWidth='xs'>
+      {showSuccess()}
+      {showError()}
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete='name'
+                onChange={handleChange('name')}
+                type='text'
+                name='name'
+                value={name}
+                variant='outlined'
+                required
+                fullWidth
+                id='name'
+                label='Full Name'
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                onChange={handleChange('email')}
+                type='email'
+                value={email}
+                autoComplete='email'
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                onChange={handleChange('password')}
+                type='password'
+                value={password}
+                autoComplete='current-password'
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+            onClick={clickSubmit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link href='/signin' variant='body2'>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-      <button onClick={clickSubmit} className='btn btn-primary'>
-        Submit
-      </button>
-    </form>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
   );
 
   const showError = () => (
@@ -98,11 +229,7 @@ const Signup = () => {
       description='Signup to MERN E-commerce App'
       className='container col-md-8 offset-md-2'
     >
-      {showSuccess()}
-      {showError()}
       {signUpForm()}
     </Layout>
   );
-};
-
-export default Signup;
+}
