60e6565e25d3cfe346c174ed644697244e6691fb
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-08-04 16:45:14 +0600

    signin page materialised

--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,9 +1,160 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+import Menu from '../core/Menu';
 import { signin, authenticate, isAuthenticated } from '../auth';
 
-const Signin = () => {
+// const Signin = () => {
+//   const [values, setValues] = useState({
+//     email: '',
+//     password: '',
+//     error: '',
+//     loading: false,
+//     redirectToReferrer: false,
+//   });
+
+//   const { email, password, loading, error, redirectToReferrer } = values;
+//   const { user } = isAuthenticated();
+
+//   const handleChange = (name) => (event) => {
+//     setValues({ ...values, error: false, [name]: event.target.value });
+//   };
+
+//   const clickSubmit = (event) => {
+//     event.preventDefault(); // so that browser does not reload
+//     setValues({ ...values, error: false, loading: true });
+//     signin({ email, password }).then((data) => {
+//       if (data.error) {
+//         setValues({ ...values, error: data.error, loading: false });
+//       } else {
+//         authenticate(data, () => {
+//           setValues({
+//             ...values,
+//             redirectToReferrer: true,
+//           });
+//         });
+//       }
+//     });
+//   };
+
+// const signInForm = () => (
+//   <form>
+//     <div className='form-group'>
+//       <label className='text-muted'>Email</label>
+//       <input
+//         onChange={handleChange('email')}
+//         type='email'
+//         className='form-control'
+//         value={email}
+//       />
+//     </div>
+
+//     <div className='form-group'>
+//       <label className='text-muted'>Password</label>
+//       <input
+//         onChange={handleChange('password')}
+//         type='password'
+//         className='form-control'
+//         value={password}
+//       />
+//     </div>
+//     <button onClick={clickSubmit} className='btn btn-primary'>
+//       Submit
+//     </button>
+//   </form>
+// );
+
+//   const showError = () => (
+//     <div
+//       className='alert alert-danger'
+//       style={{ display: error ? '' : 'none' }}
+//     >
+//       {error}
+//     </div>
+//   );
+
+//   const showLoading = () =>
+//     loading && (
+//       <div className='alert alert-info'>
+//         <h2>Loading...</h2>
+//       </div>
+//     );
+
+//   const redirectUser = () => {
+//     if (redirectToReferrer) {
+//       if (user && user.role === 1) {
+//         return <Redirect to='/admin/dashboard' />;
+//       } else {
+//         return <Redirect to='/user/dashboard' />;
+//       }
+//     }
+//     if (isAuthenticated()) {
+//       return <Redirect to='/' />;
+//     }
+//   };
+
+//   return (
+//     <Layout
+//       title='Signup page'
+//       description='Signup to MERN E-commerce App'
+//       className='container col-md-8 offset-md-2'
+//     >
+//       {showLoading()}
+//       {showError()}
+//       {signInForm()}
+//       {redirectUser()}
+//     </Layout>
+//   );
+// };
+
+function Copyright() {
+  return (
+    <Typography variant='body2' color='textSecondary' align='center'>
+      {'Copyright © '}
+      <Link color='inherit' href='#'>
+        MERN E-commerce
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Signin() {
   const [values, setValues] = useState({
     email: '',
     password: '',
@@ -36,33 +187,6 @@ const Signin = () => {
     });
   };
 
-  const signInForm = () => (
-    <form>
-      <div className='form-group'>
-        <label className='text-muted'>Email</label>
-        <input
-          onChange={handleChange('email')}
-          type='email'
-          className='form-control'
-          value={email}
-        />
-      </div>
-
-      <div className='form-group'>
-        <label className='text-muted'>Password</label>
-        <input
-          onChange={handleChange('password')}
-          type='password'
-          className='form-control'
-          value={password}
-        />
-      </div>
-      <button onClick={clickSubmit} className='btn btn-primary'>
-        Submit
-      </button>
-    </form>
-  );
-
   const showError = () => (
     <div
       className='alert alert-danger'
@@ -92,18 +216,91 @@ const Signin = () => {
     }
   };
 
+  const classes = useStyles();
+
+  const signInForm = () => (
+    <Container component='main' maxWidth='xs'>
+      {showError()}
+      {showLoading()}
+      {redirectUser()}
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            onChange={handleChange('email')}
+            type='email'
+            value={email}
+            autoFocus
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            onChange={handleChange('password')}
+            type='password'
+            value={password}
+            autoComplete='current-password'
+          />
+          <FormControlLabel
+            control={<Checkbox value='remember' color='primary' />}
+            label='Remember me'
+          />
+          <Button
+            onClick={clickSubmit}
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href='/signup' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+
   return (
     <Layout
-      title='Signup page'
-      description='Signup to MERN E-commerce App'
+      title='Signin page'
+      description='Signin to MERN E-commerce App'
       className='container col-md-8 offset-md-2'
     >
-      {showLoading()}
-      {showError()}
       {signInForm()}
-      {redirectUser()}
     </Layout>
   );
-};
-
-export default Signin;
+}
