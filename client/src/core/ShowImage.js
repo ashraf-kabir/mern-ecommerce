6492a1a40585d9bff6a2942449d5b8581626a888
Ashraf Kabir <ashrafkabir95@gmail.com>
2021-04-25 16:35:04 +0600

    margin added on product image

--- client/src/core/ShowImage.js
+++ client/src/core/ShowImage.js
@@ -7,7 +7,7 @@ const ShowImage = ({ item, url }) => (
       src={`${API}/${url}/photo/${item._id}`}
       alt={item.name}
       className='mb-3'
-      style={{ maxHeight: '100%', maxWidth: '100%' }}
+      style={{ maxHeight: '100%', maxWidth: '100%', display: 'flex', marginLeft: 'auto', marginRight: 'auto' }}
     />
   </div>
 );
