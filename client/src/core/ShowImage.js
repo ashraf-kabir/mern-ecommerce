71e6ecc626787e1e53ae9ac3a533742d70bfe8ba
Ashraf Kabir <ashrafkabir95@gmail.com>
2021-06-17 12:23:02 +0600

    item card design fixed

--- client/src/core/ShowImage.js
+++ client/src/core/ShowImage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { API } from '../config';
 
 const ShowImage = ({ item, url }) => (
-  <div className='product-img'>
+  <div className='product-img' style={{height: '250px'}}>
     <img
       src={`${API}/${url}/photo/${item._id}`}
       alt={item.name}
       className='mb-3'
-      style={{ maxHeight: '100%', maxWidth: '100%', display: 'flex', marginLeft: 'auto', marginRight: 'auto' }}
+      style={{ objectFit: 'contain', height: '100%', width: '100%', display: 'flex', marginLeft: 'auto', marginRight: 'auto' }}
     />
   </div>
 );
